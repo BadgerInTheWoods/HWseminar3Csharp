038b1cd59bca8e0686eb8c342b1b2d9acb923e77
AlexanderBadger <sashazateev444@gmail.com>
2023-08-28 17:31:10 +0200

    task 1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double expon()
+{
+    Console.WriteLine("insert int number A");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("insert int number B");
+    int B = Convert.ToInt32(Console.ReadLine());
+    double result = Math.Pow(A,B);
+    return result;
+}
+
+Console.Write(expon());
